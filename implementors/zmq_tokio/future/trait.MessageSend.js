313bad0319ddac7943f7f07e057117f566c82fc9
Joaquin Rosales <globojorro@gmail.com>
2018-01-14 00:36:01 -0600

    update gh-pages docs with zmq_tokio re-exporting zmq_futures types

---
+++ implementors/zmq_tokio/future/trait.MessageSend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["zmq_tokio"] = ["impl <a class=\"trait\" href=\"zmq_tokio/future/trait.MessageSend.html\" title=\"trait zmq_tokio::future::MessageSend\">MessageSend</a> for <a class=\"struct\" href=\"zmq_tokio/struct.Socket.html\" title=\"struct zmq_tokio::Socket\">Socket</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
